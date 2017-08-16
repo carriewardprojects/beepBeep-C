db065741b0b4a683106da0e675d42a505dd91226
Carrie Ward <carriewardprojects@gmail.com>
2017-08-16 13:06:12 -0700

    C++ source code

---
+++ beepBeep.cpp
@@ -0,0 +1,12 @@
+// beepBeep.cpp : A simple C++ program that causes the computer to beep
+// Carrie Ward 08/16/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This computer goes beep\a beep\a \n"<< endl;
+	return 0;
+}
+
